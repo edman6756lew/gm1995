f391077a1947c303c591af21ed36f342e96a6db1
edman6756lew <66494618+edman6756lew@users.noreply.github.com>
2020-06-05 16:42:23 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.google.com/search?source=hp&ei=nljCXpjVCv2E4-EPoeyY0A8&q=macfee+activate&oq=macfee+activate&gs_lcp=CgZwc3ktYWIQAzIECAAQCjIECAAQCjIECAAQCjIECAAQCjIECAAQCjIECAAQCjIECAAQCjIECAAQCjIECAAQCjIECAAQCjoCCAA6BQgAEIMBULsLWJEwYIEzaABwAHgAgAHHBIgB4zaSAQU0LTguNpgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwiYnM7lj73pAhV9wjgGHSE2BvoQ4dUDCAg&uact=5");
+die();
+?>
